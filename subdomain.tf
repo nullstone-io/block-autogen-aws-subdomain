52fa4d6d28b286f415c00657bfb0e4c3602e88cc
Brad Sickles <brad.sickles@gmail.com>
2021-03-04 14:04:03 -0500

    Rename delegate to delegation

--- subdomain.tf
+++ subdomain.tf
@@ -12,7 +12,7 @@ resource "aws_route53_zone" "this" {
   tags = data.ns_workspace.this.tags
 }
 
-resource "ns_subdomain_delegate" "delegate" {
+resource "ns_subdomain_delegation" "delegation" {
   subdomain_id = data.ns_subdomain.subdomain.id
   name_servers = aws_route53_zone.this.name_servers
 }
