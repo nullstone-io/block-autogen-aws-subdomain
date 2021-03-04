fa21982a46b48a45c011927023e13ba1940eeb92
Brad Sickles <brad.sickles@gmail.com>
2021-03-04 13:59:06 -0500

    Added subdomain delegation

---
+++ subdomain.tf
@@ -0,0 +1,18 @@
+data "ns_subdomain" "subdomain" {
+  name = var.subdomain
+}
+
+locals {
+  domain_name = data.ns_subdomain.subdomain.domain_name
+  subdomain   = "${var.subdomain}.${local.domain_name}"
+}
+
+resource "aws_route53_zone" "this" {
+  name = "${local.subdomain}.${local.domain_name}"
+  tags = data.ns_workspace.this.tags
+}
+
+resource "ns_subdomain_delegate" "delegate" {
+  subdomain_id = data.ns_subdomain.subdomain.id
+  name_servers = aws_route53_zone.this.name_servers
+}
