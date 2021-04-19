293b1a10aea6525ea38c7ac7ea5dfa0a7aefaa05
Brad Sickles <brad.sickles@gmail.com>
2021-04-19 10:30:31 -0400

    No longer influenced by subdomain var

--- variables.tf
+++
@@ -1,4 +0,0 @@
-variable "subdomain" {
-  type        = string
-  description = "This represents the token to prepend the input domain block (i.e. the fqdn is represented by {var.subdomain}.<domain>.)"
-}
