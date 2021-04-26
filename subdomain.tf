e80eae7cd470ccb7858fcceb0fa3a08838bcac0e
Scott Sickles <scottsickles@gmail.com>
2021-04-26 01:48:15 -0400

    updated to use the new provider contract

--- subdomain.tf
+++ subdomain.tf
@@ -1,11 +1,20 @@
-resource "ns_autogen_subdomain" "subdomain" {}
+data "ns_subdomain" "subdomain" {
+  stack = data.ns_workspace.this.stack
+  block = data.ns_workspace.this.block
+}
+
+resource "ns_autogen_subdomain" "autogen_subdomain" {
+  subdomain_id = data.ns_subdomain.id
+  env = data.ns_workspace.this.env
+}
 
 resource "aws_route53_zone" "this" {
-  name = ns_autogen_subdomain.subdomain.fqdn
+  name = ns_autogen_subdomain.autogen_subdomain.fqdn
   tags = data.ns_workspace.this.tags
 }
 
 resource "ns_autogen_subdomain_delegation" "to_aws" {
-  subdomain   = var.subdomain
+  subdomain_id = data.ns_subdomain.id
+  env = data.ns_workspace.this.env
   nameservers = aws_route53_zone.this.name_servers
 }
