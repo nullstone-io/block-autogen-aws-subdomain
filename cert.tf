31ea05af9ffa91641c5fd649593ec85c75dc2638
Brad Sickles <brad.sickles@gmail.com>
2021-05-11 09:28:22 -0400

    Fixed usage of ssl cert module

--- cert.tf
+++ cert.tf
@@ -6,7 +6,6 @@ module "cert" {
     zone_id = aws_route53_zone.this.zone_id
   }
 
-  tags = data.ns_workspace.this.tags
-
-  count = var.create_cert ? 1 : 0
+  tags    = data.ns_workspace.this.tags
+  enabled = var.create_cert
 }
