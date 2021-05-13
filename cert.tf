e2f0f48c16bfe5ed804899767cdbd7413d133721
Brad Sickles <brad.sickles@gmail.com>
2021-05-13 15:46:59 -0400

    Try different depends_on hack

--- cert.tf
+++ cert.tf
@@ -6,9 +6,9 @@ module "cert" {
     zone_id = aws_route53_zone.this.zone_id
   }
 
-  tags = data.ns_workspace.this.tags
-
   // Cannot use "depends_on" because it has a nested provider
-  // By using the resource, we can get the effect of depends_on
-  enabled = var.create_cert && length(ns_autogen_subdomain_delegation.to_aws.nameservers) > 0
+  // By using tags from ns_autogen_subdomain_delegation info, we create a TF dependency
+  tags = merge(data.ns_workspace.this.tags, { "Env" = ns_autogen_subdomain_delegation.to_aws.env })
+
+  enabled = var.create_cert
 }
