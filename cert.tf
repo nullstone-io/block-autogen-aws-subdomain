a7cedf283572cec6cc0451f86a5462bbf54597fa
Brad Sickles <brad.sickles@gmail.com>
2021-05-13 15:41:08 -0400

    Fixed depends_on

--- cert.tf
+++ cert.tf
@@ -6,8 +6,9 @@ module "cert" {
     zone_id = aws_route53_zone.this.zone_id
   }
 
-  tags    = data.ns_workspace.this.tags
-  enabled = var.create_cert
+  tags = data.ns_workspace.this.tags
 
-  depends_on = [ns_autogen_subdomain_delegation.to_aws]
+  // Cannot use "depends_on" because it has a nested provider
+  // By using the resource, we can get the effect of depends_on
+  enabled = var.create_cert && length(ns_autogen_subdomain_delegation.to_aws.nameservers) > 0
 }
